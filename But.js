a35d922098d946c7a2b49cdb8a3157f9f23757ef
Gleb_Zin <95599779+gleb199828@users.noreply.github.com>
2022-06-23 19:40:18 +1000

    Add files via upload

---
+++ But.js
@@ -0,0 +1,22 @@
+(function(){
+	$(window).scroll(function(){
+		if($(window).scrollTop() > 100) {
+			$('#scroll_top').show();
+		} else {
+			$('#scroll_top').hide();
+		}
+	});
+ 
+	$('#scroll_top').click(function(){
+		$('html, body').animate({scrollTop: 0}, 1800);
+		return false;
+	});
+});
+
+
+
+
+
+
+
+
